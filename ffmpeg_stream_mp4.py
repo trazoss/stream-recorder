dd127b121968cff5881308075026d4ba3c34e0f9
trazoss <97839358+trazoss@users.noreply.github.com>
2022-02-11 08:33:09 +1000

    Update ffmpeg_stream_mp4.py

--- ffmpeg_stream_mp4.py
+++ ffmpeg_stream_mp4.py
@@ -45,7 +45,7 @@ def stream(url):
 	print('ffmpeg stream mp4')
 	now = datetime.datetime.now()
 	#format_string = '/Users/hummingbird/Desktop/moon/scripts/ffmpeg/ffmpeg_stream_mp4.sh ' + str(url)
-	format_string = 'ffmpeg  -i ' + url + ' V:m:variant_bitrate:3396000 -c copy -bsf:a aac_adtstoasc ' + '/downloads' + '/' + str(now.hour) + str(now.minute) + str(now.second) + '.flv'
+	format_string = 'ffmpeg  -i ' + url + ' V m:variant_bitrate:3396000 -c copy -bsf:a aac_adtstoasc ' + '/downloads' + '/' + str(now.hour) + str(now.minute) + str(now.second) + '.flv'
 	command = str(format_string)
 	os.system(command)
 	return
